fde0ec6f665505042d1d4403bd40bc297d47b0b0
Sergei Akhmatdinov <sakhmatd@darkn.space>
2019-12-19 10:53:13 -0500

    remove usage of stack, results in a significant speedup

--- swege.c
+++ swege.c
@@ -16,20 +16,15 @@
 #define D_NAME entry->d_name
 
 static void find_files(const char *src_path);
-static void copy_files(void);
-static void render_md(void);
 static void usage(void);
 static void dir_err(const char *path);
 static void file_err(const char *path);
-static void file_cpy(FILE *in, FILE *out);
+static void stream_file(FILE *in, FILE *out);
 static char* retrieve_title(FILE *in);
 static char* mk_dst_path(const char *path);
-static void find_files(const char *src_path);
-static void render_md(void);
-static void copy_files(void);
-
-static Stack *md_stack;
-static Stack *file_stack;
+static void render_md(char *path);
+static void copy_file(const char *path);
+static void make_dir(const char *path);
 
 void
 dir_err(const char *path)
@@ -46,12 +41,12 @@ file_err(const char *path)
 }
 
 /**
- * Copies contents of one file to another.
+ * Streams contents of one file to another.
  * Not the fastest way to do it, there is overhead in moving
  * from user to kern space, but portable.
  **/
 void
-file_cpy(FILE *in, FILE *out)
+stream_file(FILE *in, FILE *out)
 {
         char buf[BUF_SIZE];
         size_t bytes;
@@ -106,25 +101,20 @@ find_files(const char *src_path)
                    break;
 
                 if (strstr(D_NAME, ".md")) {
-                        D_NAME[strlen(D_NAME) - 3] = 0; /* Remove the extention */
-                        snprintf(path, PATH_MAX, "%s/%s.md", src_path, D_NAME);
-                        push(md_stack, path);
-                        snprintf(path, PATH_MAX, "%s/%s.html", mk_dst_path(src_path), D_NAME);
-                        push(md_stack, path);
+                        snprintf(path, PATH_MAX, "%s/%s", src_path, D_NAME);
+                        render_md(path);
                 } else if (entry->d_type & DT_DIR) {
                         if (strcmp(D_NAME, "..") != 0 &&
                             strcmp(D_NAME, ".") != 0) {
-                                mode_t default_mode = umask(0);
                                 snprintf(path, PATH_MAX, "%s/%s", mk_dst_path(src_path), D_NAME);
-                                mkdir(path, 0755);
-                                umask(default_mode);
+                                make_dir(path);
                                 snprintf(path, PATH_MAX, "%s/%s", src_path, D_NAME);
                                 find_files(path);
                         }
                 } else if (!strstr(footer_file, D_NAME) &&
                            !strstr(header_file, D_NAME) ) {
                         snprintf(path, PATH_MAX, "%s/%s", src_path, D_NAME);
-                        push(file_stack, path);
+                        copy_file(path);
                 }
         }
 
@@ -135,55 +125,63 @@ find_files(const char *src_path)
 }
 
 void
-render_md(void)
+render_md(char *path)
 {
         FILE *header = fopen(header_file, "r");
         FILE *footer = fopen(footer_file, "r");
 
-        while (!is_empty(md_stack)) {
-                const char *dst_path = pop(md_stack);
-                const char *path = pop(md_stack);
-                const char *page_title;
-                FILE *fd = fopen(path, "r");
-                FILE *out = fopen(dst_path, "w");
+        FILE *fd = fopen(path, "r");
 
-                if (!fd)
-                        file_err(path);
-                if (!out)
-                        file_err(dst_path);
+        if (!fd)
+                file_err(path);
 
-                file_cpy(header, out);
+        path[strlen(path) - 3] = 0; /* Remove the extention */
+        snprintf(path, PATH_MAX, "%s.html", mk_dst_path(path));
 
-                if ((page_title = retrieve_title(fd)))
-                        fprintf(out, "<title>%s - %s<title>\n", page_title, site_title);
-                else
-                        fprintf(out, "<title>%s</title>\n", site_title);
+        FILE *out = fopen(path, "w");
 
-                MMIOT *md = mkd_in(fd, 0);
-                markdown(md, out, 0);
+        if (!out)
+                file_err(path);
 
-                file_cpy(footer, out);
+        stream_file(header, out);
 
-                fclose(fd);
-                fclose(out);
-        }
+        const char *page_title;
+        if ((page_title = retrieve_title(fd)))
+                fprintf(out, "<title>%s - %s<title>\n", page_title, site_title);
+        else
+                fprintf(out, "<title>%s</title>\n", site_title);
+
+        MMIOT *md = mkd_in(fd, 0);
+        markdown(md, out, 0);
+
+        stream_file(footer, out);
+
+        fclose(header);
+        fclose(footer);
+        fclose(fd);
+        fclose(out);
 }
 
 void
-copy_files(void)
+make_dir(const char *path)
 {
-        while (!is_empty(file_stack)) {
-                const char *path = pop(file_stack);
-                FILE *orig = fopen(path, "r");
-                FILE *new = fopen(mk_dst_path(path), "w");
+        mode_t default_mode = umask(0);
+        mkdir(path, 0755);
+        umask(default_mode);
+}
 
-                if (!orig)
-                        file_err(path);
-                if (!new)
-                        file_err(mk_dst_path(path));
+void
+copy_file(const char *path)
+{
+        FILE *orig = fopen(path, "r");
+        FILE *new = fopen(mk_dst_path(path), "w");
 
-                file_cpy(orig, new);
-        }
+        if (!orig)
+                file_err(path);
+        if (!new)
+                file_err(mk_dst_path(path));
+
+        stream_file(orig, new);
 }
 
 void
@@ -202,9 +200,6 @@ main(int argc, char *argv[])
         if (argc >= 2)
                 usage();
 
-        md_stack = new_stack();
-        file_stack = new_stack();
-
         /* Check if destination directory exists and create it if it doesn't. */
         mode_t default_mode = umask(0);
         DIR *dptr;
@@ -216,9 +211,4 @@ main(int argc, char *argv[])
         umask(default_mode);
 
         find_files(src_dir);
-        copy_files();
-        render_md();
-
-        free_stack(md_stack);
-        free_stack(file_stack);
 }
